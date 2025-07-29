e8de4c20aeba070f75e9624d57badb775c70d0e3
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-07-30 00:10:24 +0200

    Added task details path for the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router";
 import HomePage from "./ui/pages/HomePage/HomePage.jsx";
 import TasksPage from "./ui/pages/TasksPage/TasksPage.jsx";
 import Layout from "./ui/components/Layout/Layout/Layout.jsx";
+import TaskDetails from "../src/ui/components/Task/TaskDetails/TaskDetails.jsx";
 
 const App = () => {
     return (
@@ -11,7 +12,7 @@ const App = () => {
                 <Route path={"/"} element={<Layout/>}>
                     <Route index element={<HomePage/>}/>
                     <Route path="tasks" element={<TasksPage/>}/>
-                    <Route path="tasks/:id" element={<h1>Task Details Page</h1>}/>
+                    <Route path="tasks/:id" element={<TaskDetails/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
