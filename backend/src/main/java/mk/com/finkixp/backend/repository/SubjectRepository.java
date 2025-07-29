3314f6e912050233b8410c731e97c0c1586dcd2a
isendev <isen.osman252@gmail.com>
2025-07-29 18:15:40 +0200

    Add subject repository

--- backend/src/main/java/mk/com/finkixp/backend/repository/SubjectRepository.java
+++ backend/src/main/java/mk/com/finkixp/backend/repository/SubjectRepository.java
@@ -1,4 +1,10 @@
 package mk.com.finkixp.backend.repository;
 
-public interface SubjectRepository {
+import mk.com.finkixp.backend.model.domain.Subject;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SubjectRepository extends JpaRepository<Subject, Long> {
+
 }
