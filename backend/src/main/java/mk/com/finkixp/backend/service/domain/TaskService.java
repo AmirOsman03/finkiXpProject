b7596de93ddd9fcf478cfc3af216b74b746cffe4
isendev <isen.osman252@gmail.com>
2025-07-29 19:32:18 +0200

    Add taskService

--- backend/src/main/java/mk/com/finkixp/backend/service/domain/TaskService.java
+++ backend/src/main/java/mk/com/finkixp/backend/service/domain/TaskService.java
@@ -1,4 +1,19 @@
 package mk.com.finkixp.backend.service.domain;
 
-public interface TaskService {
+import mk.com.finkixp.backend.model.domain.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskService{
+    List<Task> findAll();
+
+    Optional<Task> findById(Long id);
+
+    Task save(Task task);
+
+    void deleteById(Long id);
+
+    Optional<Task> update(Long id, Task task);
+
 }
