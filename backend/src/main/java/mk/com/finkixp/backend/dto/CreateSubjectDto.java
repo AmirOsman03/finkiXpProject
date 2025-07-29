000c774e8a4e9abac689b8160186a8c7ebd1c9bb
isendev <isen.osman252@gmail.com>
2025-07-29 18:18:43 +0200

    Add subject dto

--- backend/src/main/java/mk/com/finkixp/backend/dto/CreateSubjectDto.java
+++ backend/src/main/java/mk/com/finkixp/backend/dto/CreateSubjectDto.java
@@ -1,4 +1,12 @@
 package mk.com.finkixp.backend.dto;
 
-public record CreateSubjectDto() {
+import mk.com.finkixp.backend.model.domain.Subject;
+
+public record CreateSubjectDto(
+        String name
+) {
+
+    public Subject toSubject() {
+        return new Subject(name);
+    }
 }
--- backend/src/main/java/mk/com/finkixp/backend/dto/DisplaySubjectDto.java
+++ backend/src/main/java/mk/com/finkixp/backend/dto/DisplaySubjectDto.java
@@ -1,4 +1,23 @@
 package mk.com.finkixp.backend.dto;
 
-public record DisplaySubjectDto() {
+import mk.com.finkixp.backend.model.domain.Subject;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public record DisplaySubjectDto(
+        Long id,
+        String name
+) {
+
+    public static DisplaySubjectDto from(Subject subject) {
+        return new DisplaySubjectDto(subject.getId(),subject.getName());
+    }
+    public static List<DisplaySubjectDto> from(List<Subject> subjectList) {
+        return subjectList.stream().map(DisplaySubjectDto::from).collect(Collectors.toList());
+    }
+
+    public Subject toSubject() {
+        return new Subject(name);
+    }
 }
