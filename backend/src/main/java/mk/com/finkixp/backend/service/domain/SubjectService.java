928e62e6fa007f70a6dcc2b9a3abc6b08727e753
isendev <isen.osman252@gmail.com>
2025-07-29 18:16:24 +0200

    Add subject service

--- backend/src/main/java/mk/com/finkixp/backend/service/domain/SubjectService.java
+++ backend/src/main/java/mk/com/finkixp/backend/service/domain/SubjectService.java
@@ -1,4 +1,19 @@
 package mk.com.finkixp.backend.service.domain;
 
+import mk.com.finkixp.backend.model.domain.Subject;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface SubjectService {
+    List<Subject> findAll();
+
+    Optional<Subject> findById(Long id);
+
+    Subject save(Subject subject);
+
+    void delete(Long id);
+
+    Optional<Subject> update(Long id,Subject subject);
+
 }
