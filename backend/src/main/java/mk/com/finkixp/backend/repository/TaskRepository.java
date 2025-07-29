ae2fcf880c1ef4d3c2ae6e2dfb57013236e53115
isendev <isen.osman252@gmail.com>
2025-07-29 19:31:40 +0200

    edit repository

--- backend/src/main/java/mk/com/finkixp/backend/repository/TaskRepository.java
+++ backend/src/main/java/mk/com/finkixp/backend/repository/TaskRepository.java
@@ -1,4 +1,9 @@
 package mk.com.finkixp.backend.repository;
 
-public interface TaskRepository {
+import mk.com.finkixp.backend.model.domain.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
 }
