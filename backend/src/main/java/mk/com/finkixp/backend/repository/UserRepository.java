536179e74f3943f7d2d511859ef24ed5d81e6034
isendev <isen.osman252@gmail.com>
2025-07-29 16:58:39 +0200

    Added UserRepository

---
+++ backend/src/main/java/mk/com/finkixp/backend/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package mk.com.finkixp.backend.repository;
+
+import mk.com.finkixp.backend.model.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByUsernameAndPassword(String username, String password);
+}
