e6ff1cd867f0ca917600edfbe9730a40f3be7542
isendev <isen.osman252@gmail.com>
2025-07-29 18:17:17 +0200

    Add subject application service

--- backend/src/main/java/mk/com/finkixp/backend/service/application/SubjectApplicationService.java
+++ backend/src/main/java/mk/com/finkixp/backend/service/application/SubjectApplicationService.java
@@ -1,4 +1,19 @@
 package mk.com.finkixp.backend.service.application;
 
+import mk.com.finkixp.backend.dto.CreateSubjectDto;
+import mk.com.finkixp.backend.dto.DisplaySubjectDto;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface SubjectApplicationService {
+    List<DisplaySubjectDto> findAll();
+
+    Optional<DisplaySubjectDto> findById(Long id);
+
+    Optional<DisplaySubjectDto> update(Long id, CreateSubjectDto createSubjectDto);
+
+    void deleteById(Long id);
+
+    DisplaySubjectDto save(CreateSubjectDto createSubjectDto);
 }
