caeafa84bce055823c081306483830a03b61466a
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-07-29 18:24:17 +0200

    Added a tailwind configuration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
 import {BrowserRouter, Route, Routes} from "react-router";
+import HomePage from "./ui/pages/HomePage/HomePage.jsx";
+import TasksPage from "./ui/pages/TasksPage/TasksPage.jsx";
+import Layout from "./ui/components/Layout/Layout/Layout.jsx";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<h1>Home Page</h1>}/>
-                <Route path="tasks" element={<h1>Tasks Page</h1>}/>
-                <Route path="tasks/:id" element={<h1>Task Details Page</h1>}/>
+                <Route path={"/"} element={<Layout/>}>
+                    <Route index element={<HomePage/>}/>
+                    <Route path="tasks" element={<TasksPage/>}/>
+                    <Route path="tasks/:id" element={<h1>Task Details Page</h1>}/>
+                </Route>
             </Routes>
         </BrowserRouter>
     )
