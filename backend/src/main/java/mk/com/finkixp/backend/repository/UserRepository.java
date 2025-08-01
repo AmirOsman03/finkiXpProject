278b86fe4a7428d97cb130bc1d25e139ae02ae94
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-01 13:43:32 +0200

    Top 10 users logic for repository

--- backend/src/main/java/mk/com/finkixp/backend/repository/UserRepository.java
+++ backend/src/main/java/mk/com/finkixp/backend/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package mk.com.finkixp.backend.repository;
 
+import mk.com.finkixp.backend.dto.UserLevelDto;
 import mk.com.finkixp.backend.model.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
@@ -12,4 +15,9 @@ public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsernameAndPassword(String username, String password);
 
     org.springframework.security.core.userdetails.User findByName(String name);
+
+    @Query("SELECT new mk.com.finkixp.backend.dto.UserLevelDto(u.username, u.level) " +
+            "FROM User u ORDER BY u.level DESC limit 10")
+    public List<UserLevelDto> findTop10ByLevel();
+
 }
