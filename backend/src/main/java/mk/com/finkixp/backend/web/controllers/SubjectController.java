96ce01cc46d7a80708df6819d49c4b3539feb398
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-05 16:04:43 +0200

    Added a auth on the add controller

--- backend/src/main/java/mk/com/finkixp/backend/web/controllers/SubjectController.java
+++ backend/src/main/java/mk/com/finkixp/backend/web/controllers/SubjectController.java
@@ -2,8 +2,9 @@ package mk.com.finkixp.backend.web.controllers;
 
 import mk.com.finkixp.backend.dto.CreateSubjectDto;
 import mk.com.finkixp.backend.dto.DisplaySubjectDto;
+import mk.com.finkixp.backend.model.domain.User;
 import mk.com.finkixp.backend.service.application.SubjectApplicationService;
-import mk.com.finkixp.backend.service.application.UserApplicationService;
+import mk.com.finkixp.backend.service.domain.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -16,9 +17,11 @@ import java.util.List;
 public class SubjectController {
 
     private final SubjectApplicationService applicationService;
+    private final UserService userService;
 
-    public SubjectController(SubjectApplicationService applicationService) {
+    public SubjectController(SubjectApplicationService applicationService, UserService userService) {
         this.applicationService = applicationService;
+        this.userService = userService;
     }
 
     @GetMapping
@@ -32,16 +35,21 @@ public class SubjectController {
     }
 
     @PostMapping("/add")
-    public ResponseEntity<DisplaySubjectDto> add(@RequestBody CreateSubjectDto createSubjectDto) {
-        return ResponseEntity.ok(applicationService.save(createSubjectDto));
+    public ResponseEntity<DisplaySubjectDto> add(
+            @RequestBody CreateSubjectDto createSubjectDto,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) {
+        User user = userService.findByUsername(userDetails.getUsername());
 
+        DisplaySubjectDto subject = applicationService.save(createSubjectDto, user);
+
+        return ResponseEntity.ok(subject);
     }
 
     @PutMapping("/edit/{id}")
     public ResponseEntity<DisplaySubjectDto> update(@PathVariable Long id, @RequestBody CreateSubjectDto createSubjectDto) {
         return applicationService.update(id, createSubjectDto).map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
-
     }
 
     @DeleteMapping("/delete/{id}")
