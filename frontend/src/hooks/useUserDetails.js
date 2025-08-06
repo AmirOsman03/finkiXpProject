0da85006a2bf68e726b1143cfed05bf70b3382ee
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-06 14:55:53 +0200

    Added a user details hook

---
+++ frontend/src/hooks/useUserDetails.js
@@ -0,0 +1,35 @@
+import {useEffect, useState} from "react";
+import userRepository from "../repository/userRepository.js";
+
+const initialState = {
+    username: '',
+    name: '',
+    surname: '',
+    role: '',
+    level: ''
+};
+
+const UseUserDetails= () => {
+    const [state, setState] = useState(initialState);
+
+    useEffect(() => {
+        userRepository
+            .me()
+            .then((response) => {
+                setState({
+                    username: response.data.username,
+                    name: response.data.name,
+                    surname: response.data.surname,
+                    role: response.data.role,
+                    level: response.data.level,
+                });
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    return state;
+};
+
+export default UseUserDetails;
