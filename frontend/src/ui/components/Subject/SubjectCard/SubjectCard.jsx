f0312224941a2d27661e0740ab0fc84d494abc4c
Amir Osman <151637627+AmirOsman03@users.noreply.github.com>
2025-08-07 16:39:44 +0200

    Implemented logic for displaying the action buttons only for ADMINS

--- frontend/src/ui/components/Subject/SubjectCard/SubjectCard.jsx
+++ frontend/src/ui/components/Subject/SubjectCard/SubjectCard.jsx
@@ -3,11 +3,13 @@ import {useNavigate} from "react-router";
 import {FaBook, FaRegStar, FaGraduationCap} from "react-icons/fa";
 import EditSubjectDialog from "../EditSubjectDialog/EditSubjectDialog.jsx";
 import DeleteSubjectDialog from "../DeleteSubjectDialog/DeleteSubjectDialog.jsx";
+import useUserDetails from "../../../../hooks/useUserDetails.js";
 
 const SubjectCard = ({subject, onUpdate, onDelete}) => {
     const navigate = useNavigate();
     const [EditSubjectDialogOpen, setEditSubjectDialogOpen] = useState(false);
     const [DeleteSubjectDialogOpen, setDeleteSubjectDialogOpen] = useState(false);
+    const user = useUserDetails();
 
     const handleClick = (id) => {
         navigate("/subject/" + id);
@@ -78,59 +80,61 @@ const SubjectCard = ({subject, onUpdate, onDelete}) => {
                         </div>
                     </div>
 
-                    <div className={"flex justify-end pt-5"}>
-                        {/* Edit button */}
-                        <button
-                            type="button"
-                            className="me-1 px-3 py-1.5 text-sm font-medium text-gray-700 transition-colors hover:bg-blue-500 hover:text-white focus:relative rounded-4xl hover:scale-110"
-                            aria-label="Edit"
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setEditSubjectDialogOpen(true);
-                            }}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-6"
+                    {user.role === 'ROLE_ADMIN' && (
+                        <div className={"flex justify-end pt-5"}>
+                            {/* Edit button */}
+                            <button
+                                type="button"
+                                className="me-1 px-3 py-1.5 text-sm font-medium text-gray-700 transition-colors hover:bg-blue-500 hover:text-white focus:relative rounded-4xl hover:scale-110"
+                                aria-label="Edit"
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setEditSubjectDialogOpen(true);
+                                }}
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
-                                />
-                            </svg>
-                        </button>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L6.832 19.82a4.5 4.5 0 0 1-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 0 1 1.13-1.897L16.863 4.487Zm0 0L19.5 7.125"
+                                    />
+                                </svg>
+                            </button>
 
-                        {/* Delete button */}
-                        <button
-                            type="button"
-                            className="px-3 py-1.5 text-sm font-medium text-gray-700 transition-colors hover:bg-red-500 hover:text-white focus:relative rounded-4xl hover:scale-110"
-                            aria-label="Delete"
-                            onClick={(e) => {
-                                e.stopPropagation();
-                                setDeleteSubjectDialogOpen(true);
-                            }}
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth="1.5"
-                                stroke="currentColor"
-                                className="size-6"
+                            {/* Delete button */}
+                            <button
+                                type="button"
+                                className="px-3 py-1.5 text-sm font-medium text-gray-700 transition-colors hover:bg-red-500 hover:text-white focus:relative rounded-4xl hover:scale-110"
+                                aria-label="Delete"
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    setDeleteSubjectDialogOpen(true);
+                                }}
                             >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                                />
-                            </svg>
-                        </button>
-                    </div>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth="1.5"
+                                    stroke="currentColor"
+                                    className="size-6"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                    />
+                                </svg>
+                            </button>
+                        </div>
+                    )}
                 </div>
             </div>
             <EditSubjectDialog
